a2f855c3abffd60d5c0005b3ec59d2dd75972b77
duridev <duridev@outlook.com>
2024-04-03 23:31:27 -0300

    debugeo del metodo toLowercase

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ let perdiste = 0;
 if (veces >= 1 && veces <= 5) {
     while (veces > 0) {
         veces--;
-        let elige = prompt("Ingresa uno de los siguientes valores:\n\n  *Piedra\n  *Papel\n  *Tijera\n" ).toLowerCase;
+        let elige = prompt("Ingresa uno de los siguientes valores:\n\n  Piedra\n  Papel\n  Tijera\n" ).toLowerCase();
 
 
         if ((elige === "piedra") || (elige === "papel") || (elige === "tijera")) {
@@ -32,17 +32,18 @@ if (veces >= 1 && veces <= 5) {
                 perdiste+=1;
             }
         }else{
-            alert("Has perdido en esta ocación porque no escribiste una opción valida. Recuerda escibir la primera letra con mayúscula.")
+            alert("Has perdido en esta ocación porque no escribiste una opción valida. Recuerda escibir Piedra, Papel o Tijera.");
         }
     }
+    if (ganaste > perdiste) {
+        alert(`Tú has ganado ${ganaste} ${ganaste == 1 ? "vez" : "veces"} y tu oponente ${perdiste} ${perdiste == 1 ? "vez" : "veces"}.\nEsto te convierte en el ganador de esta ronda.\nFelicidades!!! `);
+    } else if (perdiste > ganaste){
+        alert(`Tú has ganado ${ganaste} ${ganaste == 1 ? "vez" : "veces"} pero tu oponente te venció ${perdiste} ${perdiste == 1 ? "vez" : "veces"}!!.\nEsta vez la suerte no ha estado de tu lado.\nPero ya sabes lo que dicen. Mala suerte en el juego, buena suerte en el amor 😅 `);
+    } else {
+        alert(`Una ronda competitiva, Tú has ganado ${ganaste} ${ganaste == 1 ? "vez" : "veces"} y tu oponente te venció ${perdiste} ${perdiste == 1 ? "vez" : "veces"}.\n\nEs un Empate!!\n`);
+    };
+
 } else {
     alert('¿Qué parte de "elige un número entre 1 y 5" no entendiste...? 😑')
 }
 
-if (ganaste > perdiste) {
-    alert(`Tú has ganado ${ganaste} ${ganaste == 1 ? "vez" : "veces"} y tu oponente ${perdiste} ${perdiste == 1 ? "vez" : "veces"}.\nEsto te convierte en el ganador de esta ronda.\nFelicidades!!! `);
-} else if (perdiste > ganaste){
-    alert(`Tú has ganado ${ganaste} ${ganaste == 1 ? "vez" : "veces"} pero tu oponente te venció ${perdiste} ${perdiste == 1 ? "vez" : "veces"}!!.\nEsta vez la suerte no ha estado de tu lado.\nPero ya sabes lo que dicen. Mala suerte en el juego, buena suerte en el amor 😅 `);
-} else {
-    alert(`Una ronda competitiva, Tú has ganado ${ganaste} ${ganaste == 1 ? "vez" : "veces"} y tu oponente te venció ${perdiste} ${perdiste == 1 ? "vez" : "veces"}.\n\nEs un Empate!!\n`);
-};
